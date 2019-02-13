bf0bad7696829c5f20d5a643d89f56c431dd1acf
smithoo <smithoo7646@gmail.com>
2019-02-13 23:03:54 +0900

    feat: add utils

---
+++ src/utils/color.js
@@ -0,0 +1,13 @@
+export const color = {
+    getRandomBrightColor() {
+        const letters = 'ABCDEF';
+        const lettersLength = letters.length;
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * lettersLength)];
+        }
+        return color;
+    },
+};
+
+export default color;
---
+++ src/utils/url.js
@@ -0,0 +1,25 @@
+const validUrlRegex = /((https?):\/\/)([-가-힣]|\w)+(?:[/.:@]([-가-힣]|\w)+)+\/?([^\n]*)$/i;
+const protocolRegex = /^((http|https|ftp|mailto|tel):(?:\/\/)?)/;
+
+export const url = {
+    isValidURL(url) {
+        return validUrlRegex.test(url);
+    },
+    refineUrl(url = '') {
+        url = url.trim();
+        if (url && protocolRegex.test(url) === false) {
+            url = 'http://' + url;
+        }
+
+        if (this.isValidURL(url)) {
+            return url;
+        }
+
+        return '';
+    },
+    isDataURIImage(src) {
+        return /data:image\/png/.test(src);
+    },
+};
+
+export default url;
