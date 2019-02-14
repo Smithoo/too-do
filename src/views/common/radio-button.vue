6655c4f1409209ffaf320e2c2bd06c5c440dd57f
smithoo <smithoo7646@gmail.com>
2019-02-14 19:53:34 +0900

    WIP: radio button

--- src/views/common/radio-button.vue
+++ src/views/common/radio-button.vue
@@ -1,14 +1,88 @@
 <template>
-    <div>
-        RADIO BUTTON
-    </div>
+    <span class="radio-button">
+        <input type="radio"
+            class="radio-input"
+            :id="id"
+            :name="name"
+            :value="value"
+            :checked="checked"
+            @focus="onFocus"
+            @blur="onBlur"
+            @change="onChange">
+        <label class="radio-label"
+            :for="id">
+            <slot></slot>
+        </label>
+    </span>
 </template>
 
 <script>
 export default {
+    props: {
+        checked: {
+            type: Boolean,
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: String,
+            required: true
+        },
+    },
+    computed: {
+        id() {
+            return `radio-${this.value}`;
+        },
+    },
+    methods: {
+        onFocus(event) {
+            this.$emit('focus', event);
+        },
+        onBlur(event) {
+            this.$emit('emit', event);
+        },
+        onChange(event) {
+            this.$emit('change', this.value);
+        }
+    }
 };
 </script>
 
-<style>
-
+<style scoped>
+.radio-button {
+    position: relative;
+    display: inline-block;
+    margin-left: 4px;
+    margin-right: 4px;
+}
+.radio-input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    cursor: pointer;
+    opacity: 0;
+    z-index: 1;
+}
+.radio-input:checked~.radio-label::before {
+    background: radial-gradient(#00c73c 36%,#00ae34 40%,#fff 47%);
+}
+.radio-label::before {
+    position: relative;
+    top: -1px;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    margin-right: 7px;
+    border: 1px solid #bcbcbc;
+    border-radius: 50%;
+    background: #ffffff;
+    vertical-align: top;
+    content: '';
+}
+.radio-label {
+    display: inline-block;
+    cursor: pointer;
+}
 </style>
--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -1,10 +1,38 @@
 <template>
     <div class="home">
         Home
+        <radio-button
+            name="hello"
+            value="a"
+            v-model="selected"
+            >A</radio-button>
+        <radio-button
+            name="hello"
+            value="b"
+            v-model="selected"
+            >B</radio-button>
+        <radio-button
+            name="hello"
+            value="c"
+            v-model="selected"
+            >C</radio-button>
+        <div>
+            selected: {{ selected }}
+        </div>
     </div>
 </template>
 
 <script>
+import RadioButton from '@/views/common/radio-button.vue';
+
 export default {
+    components: {
+        RadioButton
+    },
+    data() {
+        return {
+            selected: ''
+        }
+    },
 };
 </script>
