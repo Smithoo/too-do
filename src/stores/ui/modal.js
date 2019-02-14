d6093d835b9379335ee69a08e0f1d755a71aa3ea
smithoo <smithoo7646@gmail.com>
2019-02-14 17:11:24 +0900

    feat: add modal vue & store

--- src/stores/ui/modal.js
+++ src/stores/ui/modal.js
@@ -1,7 +1,36 @@
 export default {
     namespaced: true,
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {},
+    state: {
+        show: false,
+        component: null,
+        data: null,
+    },
+    getters: {
+        show(state) {
+            return state.show;
+        },
+        component(state) {
+            return state.component;
+        },
+    },
+    mutations: {
+        show(state, { component = null, data = null }) {
+            state.data = data;
+            state.component = component;
+            state.show = true;
+        },
+        hide(state) {
+            state.show = false;
+            state.component = null;
+            state.data = null;
+        },
+    },
+    actions: {
+        show({ commit }, payload) {
+            commit('show', payload);
+        },
+        hide({ commit }) {
+            commit('hide');
+        },
+    },
 };
--- src/views/layouts/modal.vue
+++ src/views/layouts/modal.vue
@@ -1,14 +1,57 @@
 <template>
-    <div>
-        MODAL
+    <div class="modal">
+        <div class="modal-wrap" v-if="show">
+            <div class="dim" @click="hide"></div>
+            <component class="modal-component" :is="modalComponent" :modalData="modalData"></component>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    computed: {
+        show() {
+            return this.$store.state.ui.modal.show;
+        },
+        modalComponent() {
+            return this.$store.state.ui.modal.component;
+        },
+        modalData() {
+            return this.$store.state.ui.modal.data;
+        }
+    },
+    methods: {
+        hide() {
+            this.$store.dispatch('ui/modal/hide');
+        },
+    },
 };
 </script>
 
-<style>
-
+<style scoped>
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10000;
+}
+.modal-wrap {
+    width: 100vw;
+    height: 100vh;
+}
+.dim {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+}
+.modal-component {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    max-width: 1080px;
+    max-height: 80%;
+    transform: translate(-50%, -50%);
+    margin: 0 auto;
+    overflow: scroll;
+}
 </style>
