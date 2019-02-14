f6bd39aba75f488b7660ac0f5f4fa91abc480408
smithoo <smithoo7646@gmail.com>
2019-02-14 17:03:30 +0900

    feat: add snackbar vue & store

--- src/stores/ui/snackbar.js
+++ src/stores/ui/snackbar.js
@@ -1,7 +1,39 @@
 export default {
     namespaced: true,
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {},
+    state: {
+        show: false,
+        msg: '',
+        style: '', // ['info', 'warn', 'error']
+        timeout: null,
+    },
+    mutations: {
+        show(state, { msg = '', style = 'info', timeout = null }) {
+            state.msg = msg;
+            state.style = style;
+            state.timeout = timeout;
+            state.show = true;
+        },
+        hide(state) {
+            state.show = false;
+            if (state.timeout) {
+                clearTimeout(state.timeout);
+                state.timeout = null;
+            }
+        },
+    },
+    actions: {
+        show({ commit, state }, payload) {
+            const duration = payload.duration ? payload.duration : 2500;
+            const timeout = setTimeout(() => {
+                if (state.show) {
+                    commit('hide');
+                }
+            }, duration);
+
+            commit('show', Object.assign({}, payload, { timeout }));
+        },
+        hide({ commit }) {
+            commit('hide');
+        },
+    },
 };
--- src/views/layouts/snackbar.vue
+++ src/views/layouts/snackbar.vue
@@ -1,14 +1,72 @@
 <template>
-    <div>
-        SNACKBAR
+    <div class="snackbar">
+        <button type="button"
+            class="snackbar-btn"
+            :class="[style, { shown: shown }]"
+            @click="hide">
+            {{ msg }}
+        </button>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            shown: false,
+        };
+    },
+    computed: {
+        show() {
+            return this.$store.state.ui.snackbar.show;
+        },
+        msg() {
+            return this.$store.state.ui.snackbar.msg;
+        },
+        style() {
+            return this.$store.state.ui.snackbar.style;
+        },
+    },
+    watch: {
+        show(value) {
+            this.shown = value;
+        },
+    },
+    methods: {
+        hide() {
+            this.$store.dispatch('ui/snackbar/hide');
+        },
+    },
 };
 </script>
 
-<style>
-
+<style scoped>
+.snackbar {
+    position: fixed;
+    top: -20px;
+    width: 100vw;
+    text-align: center;
+    z-index: 9000;
+}
+.snackbar-btn {
+    padding: 4px 16px;
+    background-color: #ffffff;
+    border-radius: 1px;
+    border: 1px solid #bbbbbb;
+    font-size: 15px;
+    transform: translateY(-120px);
+    transition: transform .4s;
+}
+.snackbar-btn.info {
+    color: dodgerblue;
+}
+.snackbar-btn.warn {
+    color: orange;
+}
+.snackbar-btn.error {
+    color: red;
+}
+.snackbar-btn.shown {
+    transform: translateY(30px);
+}
 </style>
