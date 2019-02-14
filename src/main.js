af88066c6fb45a9bb1ec6f8abeb68b43623ee67e
smithoo <smithoo7646@gmail.com>
2019-02-14 14:01:53 +0900

    refact: bootstrapping

--- src/main.js
+++ src/main.js
@@ -1,15 +1,39 @@
-import './assets/css/reset.css';
-import './assets/css/common.css';
-
 import Vue from 'vue';
 import App from './app.vue';
 import router from './router';
 import store from './stores';
+import filters from './filters';
 
 Vue.config.productionTip = false;
 
-new Vue({
-    router,
-    store,
-    render: h => h(App),
-}).$mount('#app');
+class TooDo {
+    async bootstrap() {
+        await this.loadStaticResources();
+        this.addVueFilters();
+        this.initView();
+    }
+
+    loadStaticResources() {
+        const style = import(/* webpackChunkName: "style" */ './loaders/style-loader');
+
+        return Promise.all([style]);
+    }
+
+    addVueFilters() {
+        const filterEntry = Object.entries(filters);
+
+        filterEntry.forEach(([filterName, filter]) => {
+            Vue.filter(filterName, filter);
+        });
+    }
+
+    initView() {
+        new Vue({
+            router,
+            store,
+            render: h => h(App),
+        }).$mount('#app');
+    }
+}
+
+new TooDo().bootstrap();
