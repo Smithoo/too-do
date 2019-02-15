a17831f93d66bf0194ed2429db863a9435da3a32
smithoo <smithoo7646@gmail.com>
2019-02-15 09:58:26 +0900

    WIP: check-box

--- src/views/common/check-box.vue
+++ src/views/common/check-box.vue
@@ -1,14 +1,53 @@
 <template>
-    <div>
-        CHECK BOX
-    </div>
+    <span class="check-box">
+        <input type="checkbox"
+            class="checkbox-input"
+            :id="id"
+            :value="value"
+            :checked="checked"
+            :disabled="disabled"
+            @focus="onFocus"
+            @blur="onBlur"
+            @change="onChange">
+        <label class="checkbox-label"
+            :for="id">
+            <slot></slot>
+        </label>
+    </span>
 </template>
 
 <script>
 export default {
+    model: {
+        prop: 'checked',
+        event: 'change',
+    },
+    props: ['checked', 'disabled', 'value'],
+    computed: {
+        id() {
+            return `checkbox-${this.value}`;
+        },
+    },
+    mounted() {
+        // if (this.checked || this.val === this.value) {
+        //     this.isChecked = true;
+        //     this.$emit('change', this.value);
+        // }
+    },
+    methods: {
+        onFocus(event) {
+            this.$emit('focus', event);
+        },
+        onBlur(event) {
+            this.$emit('emit', event);
+        },
+        onChange(event) {
+            this.$emit('change', event.target.checked);
+        },
+    },
 };
 </script>
 
-<style>
+<style scoped`>
 
 </style>
--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -1,19 +1,44 @@
 <template>
     <div class="home">
         Home
+        <div id='example-3'>
+        <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+        <label for="jack">Jack</label>
+        <input type="checkbox" id="john" value="John" v-model="checkedNames">
+        <label for="john">John</label>
+        <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+        <label for="mike">Mike</label>
+        <br>
+        <span>Checked names: {{ checkedNames }}</span>
+        </div>
+        <check-box
+            value="a"
+            v-model="selected">
+            A
+        </check-box>
+        <check-box
+            value="b"
+            v-model="selected">
+            B
+        </check-box>
+        <div>
+            selected: {{ selected }}
+        </div>
     </div>
 </template>
 
 <script>
-// import RadioButton from '@/views/common/radio-button.vue';
+import CheckBox from '@/views/common/check-box.vue';
 
 export default {
     components: {
+        CheckBox,
         // RadioButton,
     },
     data() {
         return {
-            selected: '',
+            selected: [],
+            checkedNames: [],
         };
     },
 };
