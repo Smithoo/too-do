8a6a2f74f7049ca03ef95e82db6dd8522bf3575a
smithoo <smithoo7646@gmail.com>
2019-02-17 02:25:04 +0900

    WIP: select-box

--- src/views/common/select-box.vue
+++ src/views/common/select-box.vue
@@ -1,14 +1,69 @@
 <template>
-    <div>
-        SELECT BOX
+    <div class="select-box" :class="{ open: optionsOn }">
+        <div class="select-box-selected"
+            @click="optionsOn = !optionsOn">
+            {{ selectedLabel }}
+        </div>
+        <ul class="select-options">
+            <li class="select-option"
+                v-for="(item, index) in items"
+                :key="item.label"
+                @click="selectOption(index)">
+                {{ item.label }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        items: {
+            type: Array,
+        },
+    },
+    data() {
+        return {
+            optionsOn: false,
+            selected: null,
+        };
+    },
+    computed: {
+        selectedLabel() {
+            return this.selected ? this.selected.label : '';
+        },
+    },
+    methods: {
+        selectOption(index) {
+            const item = this.items[index];
+
+            if (this.selected && this.selected.value === item.value) {
+                this.optionsOn = false;
+                return;
+            }
+
+            this.selected = item;
+            this.$emit('input', item.value);
+            this.optionsOn = false;
+        },
+    },
 };
 </script>
 
-<style>
-
+<style scoped>
+.select-box-selected {
+    border: 1px solid #ddd;
+    min-height: 30px;
+}
+.select-options {
+    display: none;
+    border: 1px solid #ddd;
+}
+.select-box.open .select-options {
+    display: block;
+}
+.select-option:hover {
+    background-color: blue;
+    color: #ffffff;
+}
 </style>
--- src/views/pages/home.vue
+++ src/views/pages/home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         Home
+        <select-box :items="items" v-model="selected"></select-box>
         <div>
             selected: {{ selected }}
         </div>
@@ -8,13 +9,20 @@
 </template>
 
 <script>
-// import CheckBox from '@/views/common/check-box.vue';
+import SelectBox from '@/views/common/select-box.vue';
 
 export default {
     components: {
+        SelectBox,
     },
     data() {
         return {
+            items: [
+                { label: 'A', value: 'a' },
+                { label: 'B', value: 'b' },
+                { label: 'C', value: 'c' },
+                { label: 'D', value: 'd' },
+            ],
             selected: null,
         };
     },
