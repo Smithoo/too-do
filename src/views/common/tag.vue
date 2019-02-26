202d4a823479e9b95160d16b7493556948f8be8f
smithoo <smithoo7646@gmail.com>
2019-02-26 22:14:07 +0900

    feat: create tag.vue

---
+++ src/views/common/tag.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="tag" :style="{ backgroundColor: color }">
+        <div class="hide">
+            <slot>tag contents</slot>
+        </div>
+        <span>{{ sharpText }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        color: {
+            type: String,
+            default: '#dddddd',
+        },
+    },
+    computed: {
+        sharpText() {
+            const text = this.$slots.default[0].text;
+            return '#' + text;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.tag {
+    padding: 6px 8px 4px 8px;
+    border: 0;
+    border-radius: 12px;
+    height: 10px;
+    color: #3f3f3f;
+    margin-right: 4px;
+    margin-bottom: 6px;
+    cursor: pointer;
+    max-width: 50px;
+    text-overflow: ellipsis;
+    font-size: 10px;
+    display: inline-block;
+    cursor: pointer;
+}
+.tag span {
+    position: relative;
+    top: -1px;
+}
+</style>
